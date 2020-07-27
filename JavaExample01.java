fab5a302f67e3a482ed0b45409af91a9088dd70f
Imran Rashid <66275589+imran33bd@users.noreply.github.com>
2020-07-26 23:59:09 -0400

    Add files via upload

---
+++ JavaExample01.java
@@ -0,0 +1,22 @@
+public class JavaExamples01{
+ 
+  public static void main(String[] args){
+
+     System.out.println("Hello World");
+
+
+     
+
+  }
+
+
+
+  }
+
+
+
+
+
+
+
+}
---
+++ JavaExamples01.java
@@ -0,0 +1,17 @@
+public class JavaExamples01{
+
+  public static void main(String[] args){
+
+    System.out.println("Hello World");
+
+
+
+
+    }
+
+
+
+
+
+
+}
